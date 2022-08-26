31c0ba3344135d507147f77eb04839e3df2109cb
ashu.cse.sln@gmail.com <ashu.cse.sln@gmail.com>
2022-08-26 17:59:06 +0530

    User Case Two Completed Successfully

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-flip=$((RANDOM%2));
-if [ $flip -eq 0 ]
-then
-	echo "HEAD";
-else 
-	echo "TAIL";
-fi
+read -p "How Many time You Want To Flip Coin  " n;
+
+for (( i=0; i<$n; i++ ))
+do 
+	flip=$((RANDOM%2));
+	if [ $flip -eq 0 ]
+	then
+		((head++));
+	else
+		((tail++));
+	fi
+
+done
+echo "Head won $head times ";
+echo "Tail won $tail times ";
